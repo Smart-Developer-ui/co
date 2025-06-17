81993d9b9b0280bb609b1adbba2e8b7501138052
Aamir <smartdeveloper101@gmail.com>
2025-06-17 15:24:27 +0500

    Create web layout

---
+++ index.js
@@ -0,0 +1,27 @@
+/* testimonial.js */
+const swiper = new Swiper(".testimonials-swiper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  /* Responsive slidesPerView */
+  breakpoints: {
+    0:   { slidesPerView: 1 },  // Mobile
+    600: { slidesPerView: 2 },  // Small tablets
+    992: { slidesPerView: 3 },  // Desktop
+  },
+});
