c75a7b0f46fc79c9af8f75ee64aabfe137c7b007
Aamir <smartdeveloper101@gmail.com>
2025-06-17 16:46:25 +0500

    modify changes

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-<<<<<<< HEAD
+
+// /* testimonial.js */
+// const swiper = new swiper(".testimonials-swiper", {
+//   loop: true,
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 4000,
+//     disableOnInteraction: false,
+//   },
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+
+//   /* Responsive slidesPerView */
+//   breakpoints: {
+//     0:   { slidesPerView: 1 },  // Mobile
+//     600: { slidesPerView: 2 },  // Small tablets
+//     992: { slidesPerView: 3 },  // Desktop
+//   },
+// });
+// /* testimonial.js */
+// const swiper = new Swiper(".testimonials-swiper", {
+//   loop: true,
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 4000,
+//     disableOnInteraction: false,
+//   },
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+
+//   /* Responsive slidesPerView */
+//   breakpoints: {
+//     0:   { slidesPerView: 1 },  // Mobile
+//     600: { slidesPerView: 2 },  // Small tablets
+//     992: { slidesPerView: 3 },  // Desktop
+//   },
+// });
+
+
 /* testimonial.js */
-const swiper = new Swiper(".testimonials-swiper", {
+const testimonialSwiper = new Swiper(".testimonials-swiper", {
   loop: true,
   spaceBetween: 30,
   centeredSlides: true,
@@ -26,32 +82,3 @@ const swiper = new Swiper(".testimonials-swiper", {
     992: { slidesPerView: 3 },  // Desktop
   },
 });
-=======
-/* testimonial.js */
-const swiper = new Swiper(".testimonials-swiper", {
-  loop: true,
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  /* Responsive slidesPerView */
-  breakpoints: {
-    0:   { slidesPerView: 1 },  // Mobile
-    600: { slidesPerView: 2 },  // Small tablets
-    992: { slidesPerView: 3 },  // Desktop
-  },
-});
->>>>>>> second
